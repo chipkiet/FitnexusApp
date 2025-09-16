27dbc9b9a59c14f1584be534d9b4d6127b7e6c70
chipkier <hoccuakiet@gmail.com>
2025-09-16 12:26:41 +0700

    chore: initial project setup for monorepo

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="flex h-screen items-center justify-center bg-slate-800">
+            <h1 className="text-3xl font-bold text-sky-400">
+                Fitnexus Frontend Setup Complete!
+            </h1>
+        </div>
+    )
+}
+
+export default App
